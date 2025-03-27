db9be636a765b1d30a779571d6394e1824693aa0
Pittab <peter_rimmer@outlook.com>
2025-03-27 14:05:12 +0000

    optimise code and also add custom sources

--- main.py
+++ main.py
@@ -2,15 +2,34 @@ import json
 import os
 import requests
 #temp list of packages
-url = 'https://eyescary-development.github.io/CDN/agpm_packages/packagelist.json'
+url = ''
+metapath = os.path.join(os.path.expanduser('~'), '.agpm', 'localmetadata.json')
+settingspath = os.path.join(os.path.expanduser('~'), '.agpm', 'sources.escnf')
+
+def fetchsource():
+    try:
+      with open(settingspath, 'r') as file:
+        source = file.readline()
+        print("Source url: " + source)
+        return source
+    except Exception:
+        return "https://eyescary-development.github.io/CDN/agpm_packages/packagelist.json"
+
+def setsource():
+    with open(settingspath, 'w') as file:
+        file.write(input("Input your new source url: "))
+
 def fetchlist():
     response = requests.get(url)
     response.raise_for_status()
     return response.json()
 
+def fetchlocalmet():
+    with open(metapath, 'r') as f:
+        localmetadata = json.load(f)
+    return localmetadata
+
 def checkpackagelist(item):
-    response = requests.get(url)
-    response.raise_for_status()
     pkglist = fetchlist()
     try:
         temp=pkglist[item]
@@ -24,42 +43,37 @@ def lookup(item):
     print("description: " + str(metadata[item]["description"]))
     print("latest release notes: " + str(metadata[item]["releaseNotes"]))
 
+def metawrite(metadata, path):
+    with open(path, 'w') as f:
+        json.dump(metadata, f, indent=2)
+
 def install(item):
     os.system("curl -O https://eyescary-development.github.io/CDN/agpm_packages/"+item+"/protocols/install.sh && bash install.sh && rm install.sh")
-    path=os.path.join(os.path.expanduser('~'), '.agpm', 'localmetadata.json')
     try:
       with open(path, 'r') as f:
         localmetadata = json.load(f)
     except Exception:
-      with open(path, 'w') as f:
-          f.write("{}")
-      localmetadata = {}
+        localmetadata = {}
+        metawrite(localmetadata, metapath)
     cloudmetadata=fetchlist()
     localmetadata[item]=cloudmetadata[item]
-    with open(path, 'w') as f:
-        json.dump(localmetadata, f, indent=2)
+    metawrite(localmetadata, metapath)
 
 def uninstall(item):
     os.system("curl -O https://eyescary-development.github.io/CDN/agpm_packages/"+item+"/protocols/uninstall.sh && bash uninstall.sh && rm uninstall.sh")
-    file_path=os.path.join(os.path.expanduser('~'), '.agpm', 'localmetadata.json')
-    with open(file_path, 'r') as f:
-        localmetadata=json.load(f)
+    localmetadata = fetchlocalmet()
     localmetadata.pop(item, None)
-    with open(file_path, 'w') as f:
-        json.dump(localmetadata, f, indent=2)
+    metawrite(localmetadata, metapath)
 
 def update(item):
     metadata=fetchlist()
     cloudver = metadata[item]["version"]
-    file_path = os.path.join(os.path.expanduser('~'), '.agpm', 'localmetadata.json')
-    with open(file_path, 'r') as f:
-        localmetadata = json.load(f)
+    localmetadata = fetchlocalmet()
     localver = localmetadata[item]["version"]
     if localver != cloudver:
         os.system("curl -O https://eyescary-development.github.io/CDN/agpm_packages/"+item+"/protocols/update.sh && bash update.sh && rm update.sh")
         localmetadata[item]=cloudmetadata[item]
-        with open(file_path, 'w') as f:
-            json.dump(localmetadata, f, indent=2)
+        metawrite(localmetadata, metapath)
     else:
         print("Package already up to date, command already satisfied")
 
@@ -78,7 +92,14 @@ def operate(task, app):
         print("package doesn't exist :(")
 
 def main():
-    while True:
-        task,app = input("operation: "), input("app to operate: ")
-        operate(task,app)
+    global url
+    url = fetchsource()
+    usin = input("set (s)ource, or install/lookup/update/uninstall a package (o)?: ")
+    match usin:
+        case 'o':
+          while True:
+            task,app = input("operation: "), input("app to operate: ")
+            operate(task,app)
+        case 's':
+            setsource()
 main()
