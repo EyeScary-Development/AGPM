3e5e0442fee1e7db73b9636153604520dd7b23ed
Pittab <peter_rimmer@outlook.com>
2025-03-28 11:26:47 +0000

    small fix

--- main.py
+++ main.py
@@ -1,36 +1,42 @@
+import sys
 import json
 import os
 import requests
 #temp list of packages
+plisturl = ''
 url = ''
 metapath = os.path.join(os.path.expanduser('~'), '.agpm', 'localmetadata.json')
 settingspath = os.path.join(os.path.expanduser('~'), '.agpm', 'sources.escnf')
 
 def fetchsource():
+    print("fetching source list...")
     try:
       with open(settingspath, 'r') as file:
         source = file.readline()
         print("Source url: " + source)
-        return source + 'packagelist.json'
+        return source
     except Exception:
         return "https://eyescary-development.github.io/CDN/agpm_packages/packagelist.json"
 
-def setsource():
+def setsource(srcurl):
     with open(settingspath, 'w') as file:
-        file.write(input("Input your new source url (don't include the packagelist.json, that gets added): "))
+        file.write(srcurl)
 
 def fetchlist():
+    print("fetching cloud metadata...")
     response = requests.get(url)
     response.raise_for_status()
     return response.json()
 
 def fetchlocalmet():
+    print("fetch local metadata...")
     with open(metapath, 'r') as f:
         localmetadata = json.load(f)
     return localmetadata
 
 def checkpackagelist(item):
     pkglist = fetchlist()
+    print("checking package list...")
     try:
         temp=pkglist[item]
         return True
@@ -48,7 +54,7 @@ def metawrite(metadata, path):
         json.dump(metadata, f, indent=2)
 
 def install(item):
-    os.system("curl -O https://eyescary-development.github.io/CDN/agpm_packages/"+item+"/protocols/install.sh && bash install.sh && rm install.sh")
+    os.system("curl -O"+url+item+"/protocols/install.sh && bash install.sh && rm install.sh")
     try:
       with open(path, 'r') as f:
         localmetadata = json.load(f)
@@ -60,7 +66,7 @@ def install(item):
     metawrite(localmetadata, metapath)
 
 def uninstall(item):
-    os.system("curl -O https://eyescary-development.github.io/CDN/agpm_packages/"+item+"/protocols/uninstall.sh && bash uninstall.sh && rm uninstall.sh")
+    os.system("curl -O"+url+item+"/protocols/uninstall.sh && bash uninstall.sh && rm uninstall.sh")
     localmetadata = fetchlocalmet()
     localmetadata.pop(item, None)
     metawrite(localmetadata, metapath)
@@ -71,7 +77,7 @@ def update(item):
     localmetadata = fetchlocalmet()
     localver = localmetadata[item]["version"]
     if localver != cloudver:
-        os.system("curl -O https://eyescary-development.github.io/CDN/agpm_packages/"+item+"/protocols/update.sh && bash update.sh && rm update.sh")
+        os.system("curl -O"+url+item+"/protocols/update.sh && bash update.sh && rm update.sh")
         localmetadata[item]=cloudmetadata[item]
         metawrite(localmetadata, metapath)
     else:
@@ -88,12 +94,16 @@ def operate(task, app):
                 update(app)
             case "search":
                 lookup(app)
+            case 'srcset':
+                setsource(app)
+
     else:
-        print("package doesn't exist :(")
+        print("package doesn't exist, terminating...")
 
 def main():
-    global url
-    url = fetchsource()
+    global plisturl, url
+    plisturl=fetchsource() + 'packagelist.json'
+    url=fetchsource()
     usin = input("set (s)ource, or install/lookup/update/uninstall a package (o)?: ")
     match usin:
         case 'o':
